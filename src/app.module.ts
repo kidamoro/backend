62bc5fc641831940031e2985267257ae8f954cee
Enrique Montaño <enrique@rebellionpay.com>
2021-10-13 12:38:40 +0200

    Create MongoDB integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseConfigService } from './config/MongooseConfigService';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    }),
+    MongooseModule.forFeature([
+      //   // { name: Document1.name, schema: documentSchema1 },
+      //   // { name: Document2.name, schema: documentSchema2 },
+    ]),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        MONGODB_URI: Joi.string().uri().required(),
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
