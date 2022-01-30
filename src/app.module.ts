66fa9da3882d9dbb5446ce577f7985b6ded396f0
Enrique Montaño <enrique@rebellionpay.com>
2022-01-30 20:16:34 +0100

    Remove unuseful import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import * as Joi from 'joi';
 import { AppController } from './app.controller';
