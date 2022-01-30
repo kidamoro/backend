a2e75f915863872246dd217c3772c117bc115cf7
Enrique Montaño <enrique@rebellionpay.com>
2022-01-30 19:39:08 +0100

    Fix database module exporting database provider

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { DatabaseProvider } from './database.provider';
 
-@Module({})
+@Module({
+  imports: [DatabaseProvider],
+  exports: [DatabaseProvider],
+})
 export class DatabaseModule {}
