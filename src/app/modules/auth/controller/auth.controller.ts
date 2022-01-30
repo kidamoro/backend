c8eb56ff664f34c4e627bfd0a2a053880ccde94c
Dayan Rojas <dayan.rojas@kubide.io>
2022-01-30 13:25:06 +0100

    Implementing JSON web tokens validations

--- src/app/modules/auth/controller/auth.controller.ts
+++ src/app/modules/auth/controller/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { AccessToken } from 'src/shared/interfaces/interfaces/user/auth/jwt-payload.interfaces';
 import { AuthCredentialsDto } from '../dto/auth-credentials.dto';
 import { CreateAuthDto } from '../dto/create-auth.dto';
 import { UpdateAuthDto } from '../dto/update-auth.dto';
@@ -7,11 +8,17 @@ import { AuthService } from '../service/auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly usersService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('/signup')
   create(
     @Body() authCredentialsDto: AuthCredentialsDto) {
-    return this.usersService.signUp(authCredentialsDto);
+    return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  singIn(
+    @Body() authCredentialsDto: AuthCredentialsDto):Promise<AccessToken> {
+    return this.authService.signIn(authCredentialsDto);
   }
 }
