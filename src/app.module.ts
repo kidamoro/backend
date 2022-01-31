24fabc289b1f600c5b525ef2cf2c66cff9eea7ea
Enrique Montaño <enrique@rebellionpay.com>
2022-01-31 07:33:05 +0100

    Remove joi dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import * as Joi from 'joi';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'production', 'test')
-          .default('development'),
-        PORT: Joi.number().default(3000),
-      }),
-    }),
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
 })
 export class AppModule {}
