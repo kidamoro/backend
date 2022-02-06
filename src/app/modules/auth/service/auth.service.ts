7884e15f5518067d490a1cb39ab86fd61ebe8c95
Dayan Rojas <dayan.rojas@kubide.io>
2022-02-06 20:58:48 +0100

    style: Format & linter fixes

--- src/app/modules/auth/service/auth.service.ts
+++ src/app/modules/auth/service/auth.service.ts
@@ -1,8 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from '../dto/auth-credentials.dto';
-import { CreateAuthDto } from '../dto/create-auth.dto';
-import { UpdateAuthDto } from '../dto/update-auth.dto';
 import { UserRepository } from '../repository/users.repository';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
@@ -11,23 +9,23 @@ import { AccessToken, JwtPayload } from 'src/shared/interfaces/interfaces/user/a
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository( UserRepository)
+    @InjectRepository(UserRepository)
     private userRepository: UserRepository,
     private jwtService: JwtService,
-  ){}
-  async signUp(authCredentialsDto: AuthCredentialsDto):Promise<void>{
+  ) { }
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto:AuthCredentialsDto):Promise<AccessToken>{
-    const {username, password}= authCredentialsDto;
-    const user = await this.userRepository.findOne({username});
-    if(user && (await bcrypt.compare(password, user.password))){
-       const payload: JwtPayload = {username};
-       const accessToken: string = await this.jwtService.sign(payload);
-       return {accessToken};
-    }else {
-      throw new UnauthorizedException('Please check yoy login credentials')
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<AccessToken> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepository.findOne({ username });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Please check yoy login credentials');
     }
   }
 }
